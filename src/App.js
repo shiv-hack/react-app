11de064e7aee000e09953dc9061a05c973db0ec0
Shivam Garg <s.smartshivam@gmail.com>
2019-10-27 11:10:00 +0530

    Initial code commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+	state = {
+		characters: []
+	}
+	render() {
+		return (
+			<div className="container">
+			</div>
+		)
+	}
+}
+
+export default App
